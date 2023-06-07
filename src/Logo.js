47d8ac1e25401bd6c6466521eab7a06443eddcdf
caleigh <caleighsteill@outlook.com>
2023-06-06 21:50:39 -0700

    logo and bg

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LogoImage from "./CSlogo.png";
+
+
+const Logo = () => {
+    return(
+    <div>
+        <img className="logo-image" src={LogoImage} alt="Caleigh Steill"/>
+    </div>
+    )
+}
+
+export default Logo;
